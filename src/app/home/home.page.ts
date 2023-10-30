d13757157b64f4638777707796a59147ace910a6
Ivan <van-petryk@ukr.net>
2023-10-30 08:26:25 +0100

    styling

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonFooter, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCol, IonItem, IonLabel, IonInput, IonList } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonFooter, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCol,
+    IonList, IonItem, IonLabel, IonInput ],
 })
 export class HomePage {
   constructor() {}
